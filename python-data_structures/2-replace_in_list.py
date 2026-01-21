8ba3c07cf37b766b2ea8ac81dc03fc8f5875df47
SaraEstelle <12152@holbertonstudents.com>
2026-01-21 10:58:27 +0100

    adding the specifier

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -6,3 +6,10 @@ def replace_in_list(my_list, idx, element):
         return my_list
     my_list[idx] = element
     return my_list
+#code simplified based on feedback
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
