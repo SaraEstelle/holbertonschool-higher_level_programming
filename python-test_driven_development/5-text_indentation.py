44f01d4925e6e9f08ef1744d4f0dfd58b5e13bbe
SaraEstelle <12152@holbertonstudents.com>
2026-01-23 21:31:28 +0100

    ajusting the code

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,18 +14,21 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
-    separators = ['.', '?', ':']
-    buffer = ""
+    new_line = False
 
     for char in text:
-        buffer += char
-        if char in separators:
-            print(buffer.strip())
-            print()
-            buffer = ""
+        if new_line is False:
+            if char == ' ':
+                continue
+            else:
+                new_line = True
 
-    if buffer.strip():
-        print(buffer.strip())
+        if char in ".?:":
+            print(char)
+            print()
+            new_line = False
+        else:
+            print(char, end="")
