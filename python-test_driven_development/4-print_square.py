3c0fc5f8232b6c30774f891ba0c064e24e247d4b
SaraEstelle <12152@holbertonstudents.com>
2026-01-23 18:36:20 +0100

    ajistong the float negatif case on the code

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -12,8 +12,12 @@ def print_square(size):
         TypeError: if size is not an integer.
         ValueError: if size < 0.
     """
-    if type(size) is not int:
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
 
