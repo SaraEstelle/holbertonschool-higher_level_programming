daac09cd7520ad62cde44d26f9dfb3990c6be6a4
SaraEstelle <12152@holbertonstudents.com>
2026-01-23 10:52:12 +0100

    writning the TDD for adding integer functions

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module provides a function that adds two integers.
+It accepts integers and floats.
+Floats are cast to integers before addition.
+Invalid types raise a TypeError.
+"""
+
+
+def add_integer(a, b=98):
+    """Add two integers.
+
+    Floats are cast to integers.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
