49d8f299fbe1c7ab2bc1df0e091e27c0f3c4c067
SaraEstelle <12152@holbertonstudents.com>
2026-01-23 20:19:49 +0100

    correcting the pycodestyle on the file 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,10 +18,14 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
+    if isinstance(a, float) and (
+        a != a or a == float('inf') or a == float('-inf')
+    ):
         raise TypeError("a must be an integer")
 
-    if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
+    if isinstance(b, float) and (
+        b != b or b == float('inf') or b == float('-inf')
+    ):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
