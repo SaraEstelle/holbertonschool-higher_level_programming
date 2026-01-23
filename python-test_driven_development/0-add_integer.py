df7155eb4f2314ed7fa30dfc361becb4361ced90
SaraEstelle <12152@holbertonstudents.com>
2026-01-23 20:03:46 +0100

    correcting the add_integer.py file and adding tests on the .txt file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,6 +6,8 @@ Floats are cast to integers before addition.
 Invalid types raise a TypeError.
 """
 
+import math
+
 
 def add_integer(a, b=98):
     """Add two integers.
@@ -16,6 +18,12 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
 
     if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    if isinstance(a, float) and (math.isnan(a) or math.isinf(a)):
+        raise TypeError("a must be an integer")
+
+    if isinstance(b, float) and (math.isnan(b) or math.isinf(b)):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
