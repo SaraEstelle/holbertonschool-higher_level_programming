af311fc730e451afd3c2a20e42909c7a7518d85a
SaraEstelle <12152@holbertonstudents.com>
2026-01-26 14:15:00 +0100

    pushing the first tasks and completing the main project

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""modules that defines an empty square class"""
+
+
+class Square:
+    """an empty class that define a square"""
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a Square instance.
+        Args:
+            size (int): The size of the square's sides.
+        """
+        self.__size = size
