62abb81a784029b0dfe62a1a7154737bf895f946
SaraEstelle <12152@holbertonstudents.com>
2026-02-03 16:05:25 +0100

    Adding shape interface with circle and rectangle using duck typing

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,50 @@
+#!/usr/bin/ env python3
+"""Shapes, interfaces, and duck typing """
+
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """Abstract base class for geometric shapes."""
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """Circle shape implementing area and perimerter."""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def perimeter(self):
+        return 2 *math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """Rectangle shape implementing area and permimeter."""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(obj):
+    """Print area and perimeter using duck typing."""
+    print("Area:", obj.area())
+    print("Perimeter:", obj.perimeter())
