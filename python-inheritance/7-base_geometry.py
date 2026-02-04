2911b5cbde798904659905ad77bda7f7d85e88a6
SaraEstelle <12152@holbertonstudents.com>
2026-02-04 10:34:36 +0100

    correcting the type value int issu and the pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -9,11 +9,11 @@ class BaseGeometry:
 
     def area(self):
         """Raise an exception because area is not implemented."""
-        raise Exception("area()( is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Validate that value is a positive integer."""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
