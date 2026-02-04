458fba5469a3f141dc8c4d799cd590b1105f75b2
SaraEstelle <12152@holbertonstudents.com>
2026-02-04 10:24:11 +0100

    Correcting the synthax of the exception

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,4 +9,4 @@ class BaseGeometry:
 
     def area(self):
         """Raise an exception because area is not implemented."""
-        raise Exception("area()( is not implemented")
+        raise Exception("area() is not implemented")
