52906652c5e65d6dcea4ecaf2965e09239c759f0
SaraEstelle <12152@holbertonstudents.com>
2026-02-04 10:04:53 +0100

    Correcting the pycodestyle issu

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a class MyList that inherits from list."""
 
-
 class MyList(list):
     """Represents a list with additional functionality."""
     def print_sorted(self):
