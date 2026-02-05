ff0bc6192f9588971fbbde101bf93f62a8de6f85
SaraEstelle <12152@holbertonstudents.com>
2026-02-05 19:17:38 +0100

    correcting the synthax

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -28,11 +28,11 @@ class Circle(Shape):
 
     def area(self):
         """Return the area of the circle."""
-        return math.pi * (self.radius ** 2)
+        return math.pi * abs(self.radius ** 2)
 
     def perimeter(self):
         """Return the perimeter of the circle."""
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * abs(self.radius)
 
 
 class Rectangle(Shape):
@@ -52,7 +52,7 @@ class Rectangle(Shape):
         return 2 * (self.width + self.height)
 
 
-def shape_info(obj: Shape):
+def shape_info(shape):
     """Print area and perimeter using duck typing."""
-    print("Area:", obj.area())
-    print("Perimeter:", obj.perimeter())
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
