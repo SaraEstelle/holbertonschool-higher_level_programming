14cd4160a6bad1d707319fc7334102e7f9c45be7
SaraEstelle <12152@holbertonstudents.com>
2026-02-05 19:40:55 +0100

    correcting the print message

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
-"""The mystical Dragon: Mastering Mixins"""
+"""Module demonstrating the use of mixins
+to compose swimming and flying behaviors."""
 
 
 class SwimMixin:
     """Mixin class to add swimming capability."""
     def swim(self):
-        """Print a message indicating that the creature swims."""
-        print("The Creature swims!")
+        """Print a message indicating
+        that the creature swims."""
+        print("The creature swims!")
 
 
 class FlyMixin:
     """Mixin class to add flying capability."""
     def fly(self):
-        """Print a message indicating that the creature flies."""
+        """Print a message indicating
+        that the creature flies."""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
-    """Class representing a Dragon, which can both swim and fly."""
+    """Class representing a dragon, which can both swim and fly."""
     def roar(self):
         """Print a message indicating that the dragon roars."""
         print("The dragon roars!")
