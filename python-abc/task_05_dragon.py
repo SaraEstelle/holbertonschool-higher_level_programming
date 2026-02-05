83e2f387e62a0b7f78dae24029c67f4d04f64684
SaraEstelle <12152@holbertonstudents.com>
2026-02-05 19:34:39 +0100

    Adding the docstirng

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -3,15 +3,21 @@
 
 
 class SwimMixin:
+    """Mixin class to add swimming capability."""
     def swim(self):
+        """Print a message indicating that the creature swims."""
         print("The Creature swims!")
 
 
 class FlyMixin:
+    """Mixin class to add flying capability."""
     def fly(self):
+        """Print a message indicating that the creature flies."""
         print("The creature flies!")
 
 
 class Dragon(SwimMixin, FlyMixin):
+    """Class representing a Dragon, which can both swim and fly."""
     def roar(self):
+        """Print a message indicating that the dragon roars."""
         print("The dragon roars!")
