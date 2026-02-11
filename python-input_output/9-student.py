3b8e3b350182427235795869fc299f2e099e7e9d
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 17:36:13 +0100

    Ajusting the pycodestyle exigences

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-class Student:
-    """Class that defines a student."""
+"""Module that defines a Student class with JSON serialization."""
 
+
+class Student:
+    """Class that defines a student with first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """Return the dictionary representation of a Student instance.
 
-        """Returns the dictionary representation of a Student."""
+        Returns:
+            dict: Attributes of the student.
+        """
         return self.__dict__
