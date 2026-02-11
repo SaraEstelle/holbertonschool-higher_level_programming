845434b31fb0616754156d359892eeb7fab98f23
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 17:20:39 +0100

    Ajusting the docstring

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Script that adds command-line arguments to a JSON file."""
+
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 
