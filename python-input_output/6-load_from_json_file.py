04e9d5b944d68e0ce8bc7310c6dba16ea60240da
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 17:07:25 +0100

    Ajusting the docstring

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Module that defines a function to load an object from a JSON file."""
+
 import json
 
+
 def load_from_json_file(filename):
-    """Creates an object from a JSON file."""
+    """Create an object from a JSON file.
+
+    Args:
+        filename (str): The name of the file.
+
+    Returns:
+        object: The Python data structure represented in the file.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         return json.load(f)
