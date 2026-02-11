0c3a02bc0b2c1d56f5c263035ec5ab57c4507f64
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 17:42:20 +0100

    Ajusting the doc string

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
+"""Module that defines a Student class with JSON serialization and filtering."""
+
+
 class Student:
-    """Class that Defines a student"""
+    """Class that defines a student with first name, last name, and age."""
 
     def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns a dictionary representation of Student instance."""
+        """Return the dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes in this list are returned.
+
+        Args:
+            attrs (list, optional): List of attribute names to retrieve.
+
+        Returns:
+            dict: Dictionary of the student's attributes.
+        """
         if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
             return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
         return self.__dict__
