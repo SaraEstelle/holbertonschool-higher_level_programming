28e4778a3628c61b3cd2da406ad4c2fa6b2ec476
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 16:54:16 +0100

    ajusting the docstring

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Module that defines a function to save an object to a JSON file."""
+
 import json
 
+
 def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file using JSON representation."""
+    """Write an object to a text file using JSON representation.
+
+    Args:
+        my_obj: The object to serialize.
+        filename (str): The name of the file.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
