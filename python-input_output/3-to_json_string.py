7613ae3b197f851516c7c891a24d575deb726d82
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 16:31:40 +0100

    Adding the docstring

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Module that defines a function to convert an object to JSON."""
+
 import json
 
+
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object (string)."""
+    """Return the JSON representation of an object.
+
+    Args:
+        my_obj: The object to convert.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
     return json.dumps(my_obj)
