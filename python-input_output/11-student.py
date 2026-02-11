da85ca87e060b62e47e638abbc03cd9aa62de01d
SaraEstelle <12152@holbertonstudents.com>
2026-02-11 17:58:53 +0100

    Ajusting the docstring

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""Module that defines a Student class with JSON serialization and reloading."""
+
+
 class Student:
-    """Class thatt defines a Student."""
+    """Class that defines a student with first name, last name, and age."""
 
-
-    def __init__(self, fisrt_name, last_name, age):
-        self.first_name = fisrt_name
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns a dictionary representation of a student instance."""
-        if isinstance(attrs, list) and all(isinstance (x, str)for x in attrs):
+        """Retrieve a dictionary representation of the student."""
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
             return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
-        return self.__dict__
+        return dict(self.__dict__)
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance."""
+        """Replace all attributes of the Student instance with the given dictionary."""
         for key, value in json.items():
-            setattr(self, key,value)
+            setattr(self, key, value)
