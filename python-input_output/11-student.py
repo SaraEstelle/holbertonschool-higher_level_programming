db32a5e9fd96af0b9edd273d77558038f71f5faf
SaraEstelle <12152@holbertonstudents.com>
2026-02-13 20:31:52 +0100

    ajusting the pycodestyle

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Module that defines a Student class with JSON serialization and reloading."""
+"""Module that defines a Student class with
+JSON serialization and reloading.
+"""
 
 
 class Student:
-    """Class that defines a student with first name, last name, and age."""
+    """Class that defines a student with
+    first name, last name, and age.
+    """
 
     def __init__(self, first_name, last_name, age):
         """Initialize a Student instance."""
@@ -12,12 +16,21 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of the student."""
-        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
-            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        """Retrieve a dictionary representation
+        of the student.
+        """
+        if isinstance(attrs, list) and \
+                all(isinstance(x, str) for x in attrs):
+            return {
+                key: self.__dict__[key]
+                for key in attrs
+                if key in self.__dict__
+            }
         return dict(self.__dict__)
 
     def reload_from_json(self, json):
-        """Replace all attributes of the Student instance with the given dictionary."""
+        """Replace all attributes of the Student
+        instance with the given dictionary.
+        """
         for key, value in json.items():
             setattr(self, key, value)
