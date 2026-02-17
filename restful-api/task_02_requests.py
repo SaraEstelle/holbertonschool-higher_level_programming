9a9c20188ef631dc5b91ddf8dc1bdbf8ee35d0fb
SaraEstelle <12152@holbertonstudents.com>
2026-02-17 14:29:31 +0100

    initial commit

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,38 @@
+import requests
+import csv
+
+URL =  "https://jsonplaceholder.typicode.com/posts"
+
+
+def fetch_and_print_posts():
+    response = requests.get(URL)
+
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts:
+            print(post["title"])
+
+def fetch_and_save_posts():
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        posts =  response.json()
+
+        structured_data = [
+            {
+                "id": post["id"],
+                "title": post["title"],
+                "body": post["body"]
+            }
+            for post in posts
+        ]
+
+        with open("posts.csv", "w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = ["id", "title", "body"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(structured_data)
