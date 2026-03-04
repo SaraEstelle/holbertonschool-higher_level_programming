d47a2b10d262e3ff0db5d2e3aa9ec0b8f96a52aa
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 07:36:22 +0100

    Correcting the value of the table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create table second_table and insert multiple rows
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTSsecond_table (
     id INT,
     name VARCHAR(256),
     score INT
@@ -8,4 +8,4 @@ CREATE TABLE second_table (
 INSERT INTO second_table (id, name , score) VALUES ( 1, 'John', 10);
 INSERT INTO second_table (id, name , score) VALUES ( 2, 'Alex', 3);
 INSERT INTO second_table (id, name , score) VALUES ( 3, 'Bob', 14);
-INSERT INTO second_table (id, name , score) VALUES ( 4, 'George', 7);
+INSERT INTO second_table (id, name , score) VALUES ( 4, 'George', 8);
