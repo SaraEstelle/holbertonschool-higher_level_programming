c1c3a81dbe74cd465da61b52641cbea4287ac985
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 07:43:05 +0100

    Adding the database

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,15 @@
--- Convert database, table and field to utf8 (utf8mb4)
+-- Convert database hbtn_0c_0 to UTF8 (utf8mb4)
+USE hbtn_0c_0;
+
 ALTER DATABASE hbtn_0c_0
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
-ALTER TABLE second_table
-    CONVERT TO CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
