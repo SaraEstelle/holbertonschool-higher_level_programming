d3a0959552d89ef6c9baa6ea4ea962f07f40c6ef
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 13:37:41 +0100

    removing the alias

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Script that lists all genres and the number of shows linked from hbtn_0d_tvshows
 
-SELECT g.name AS genre, COUNT(tsg.show_id) AS number_of_shows
-FROM genres g
-JOIN tv_show_genres tsg ON g.id = tsg.genre_id
-GROUP BY g.name
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.name
 ORDER BY number_of_shows DESC;
