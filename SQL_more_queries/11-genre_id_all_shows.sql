eb6a70f01066e890feb3a0d9173db91041a8b6ea
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 12:46:10 +0100

    adding the OUTER for the dumps

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,6 +3,6 @@
 -- Select all shows with their genre_id (NULL if no genre)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres
+LEFT OUTER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
