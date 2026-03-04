a7b7c463a68716620be0bf8ae93562a82593ac44
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 13:39:42 +0100

    correcting the synthax

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,7 @@
 
 SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id IS NOT NULL
 GROUP BY genres.name
 ORDER BY number_of_shows DESC;
