e378328ff217e9729896b11b364ebcb4c7616792
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 12:06:22 +0100

    correcting the synthax

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,13 +1,13 @@
--- Script that creates the database hbtn_0d_usa and the table cities4
+-- Script that creates the database hbtn_0d_usa and the table cities
 
--- Create the database hbtn_0d_usa if it does not already exist
-CREATE DATABASE IF NOT EXISTS hbtn-0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Create the table cities if it does not already exist
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    CONSTRAINT fk_cities_state FOREIGN KEY (state_id)
-        REFERENCES hbtn_0d_usa.states(id)
-) ENGINE=InnoDB;
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+) ENGINE=INNODB;
