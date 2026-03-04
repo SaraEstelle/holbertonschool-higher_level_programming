8381acebe3d80d5e1e52c03fbebd17e61dfdc799
SaraEstelle <12152@holbertonstudents.com>
2026-03-04 13:20:17 +0100

    correcting task 11 and adding task 12

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,8 @@
 -- Script that lists all shows and their genre_id from hbtn_0d_tvshows
 
+-- Select all shows with their genre_id (NULL if no genre)
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Script that lists all shows that have no genre linked from hbtn_0d_tvshows
+
+-- Select show title and genre_id (NULL when no genre), ordered by title and genre_id
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
