589c65c34a04c8792db3b09638c8833ec85c3c95
SaraEstelle <12152@holbertonstudents.com>
2026-03-06 19:33:52 +0100

    Correcting the PycodeStyle

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -3,7 +3,7 @@
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
@@ -12,21 +12,19 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db = sys.argv[3]
 
-    # Connexion à la base MySQL
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, password, db),
         pool_pre_ping=True
     )
 
-    # Création de la session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Requête : join City avec State et tri par City.id
-    cities = session.query(City, State).join(State, City.state_id == State.id).order_by(City.id).all()
+    cities = session.query(City, State).join(
+        State, City.state_id == State.id
+    ).order_by(City.id).all()
 
-    # Affichage
     for city, state in cities:
-        print(f"{state.name}: ({city.id}) {city.name}")
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
