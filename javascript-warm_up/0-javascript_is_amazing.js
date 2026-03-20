e9e133f8643395e7c968d6fd09bac1bb89effb91
SaraEstelle <12152@holbertonstudents.com>
2026-03-20 15:50:40 +0100

    print javascript is amazing with a JS script

---
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// On déclare une constante appelée "myVar" et on lui assigne la valeur "JavaScript is amazing"
+// "const" signifie que la variable ne peut pas être réassignée (contrairement à "var" ou "let")
+const myVar = 'JavaScript is amazing';
+
+// On affiche le contenu de "myVar" dans la console
+// console.log() est la fonction standard pour afficher du texte en JavaScript
+console.log(myVar);
