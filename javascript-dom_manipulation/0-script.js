47176b9f3c4d5061661ef30da879106b627db4bd
SaraEstelle <12152@holbertonstudents.com>
2026-03-23 14:02:00 +0100

    Writing a JavaScript script that updates the text color of the header element to red (#FF0000)

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,7 @@
+// Séléctionne le prmier élément<header> dans le DOM
+// et le sotkce dnas une constante pour pouvoir le manipuler
+const header = document.querySelector('header');
+
+// Modifie la couleur du texte de l'éméement header en rouge
+// en utilisant la proporiété style.color avec le code héxadécimal #FF0000
+header.style.color = '#FF0000';
