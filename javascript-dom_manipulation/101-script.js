6902ca88bbe937b28c951a3cd134f139fcf64135
SaraEstelle <12152@holbertonstudents.com>
2026-03-24 12:26:46 +0100

    Writing a JavaScript script that fetches and prints how to say “Hello” depending on the language

---
+++ javascript-dom_manipulation/101-script.js
@@ -0,0 +1,26 @@
+// On attend que le DOM soit entièrement chargé
+document.addEventListener('DOMContenLoaded', function() {
+  // Séléctionner le combo box, le bouton et le div d'affichage
+  const languageCode = document.querySelector('#language_code');
+  const btnTranslate = document.querySelector('#btn_translate');
+  const hello = document.querySelector('#hello');
+
+  // Ecouter le clic sur le bouton "Translate"
+  btnTranslate.addEventListener('click', function () {
+    // Récuperer la valeur séléctionné dans le combo box ( ex: "fr , en , es")
+    const lang = languageCode.ariaValueMax;
+
+    // Construit l'URL avec le code langue séléctionné
+    fetch('https://hellosalut.stefanbohacek.com/?lang=' + lang)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        // affiche la traduction da,s le div #hello
+        hello.textContent = data.hello;
+    })
+    .catch(function (error) {
+        console.log('Erreur: ', error);
+    });
+  });
+});
